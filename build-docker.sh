6f0b0c7e316cdfee2692feacf9e95124031b6f00
Bob Steers <steers.bob@gmail.com>
2019-02-04 15:57:26 +0100

    add run cmd to dockerfile; remove squash

--- build-docker.sh
+++ build-docker.sh
@@ -8,5 +8,5 @@ cp -rf ./brewblox_ctl_lib/* ./docker/source/
 # recursively clean all pycache/pyc files
 find ./docker/source/ | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 
-docker build --no-cache --squash -f docker/amd/Dockerfile -t brewblox/brewblox-ctl-lib:local docker
-docker build --no-cache --squash -f docker/arm/Dockerfile -t brewblox/brewblox-ctl-lib:rpi-local docker
+docker build --no-cache -f docker/amd/Dockerfile -t brewblox/brewblox-ctl-lib:local docker
+docker build --no-cache -f docker/arm/Dockerfile -t brewblox/brewblox-ctl-lib:rpi-local docker
