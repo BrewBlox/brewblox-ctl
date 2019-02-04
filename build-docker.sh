e73eab7dcfdad17d9c89152ab4f9054d26dd9e09
Bob Steers <steers.bob@gmail.com>
2019-02-04 16:31:25 +0100

    build cleanup

--- build-docker.sh
+++ build-docker.sh
@@ -4,12 +4,9 @@ set -e
 rm -rf ./docker/source || true
 mkdir ./docker/source
 cp -rf ./brewblox_ctl_lib/* ./docker/source/
-ls ./docker/source
 
 # recursively clean all pycache/pyc files
 find ./docker/source/ | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
 docker build --no-cache -f docker/amd/Dockerfile -t brewblox/brewblox-ctl-lib:local docker
 docker build --no-cache -f docker/arm/Dockerfile -t brewblox/brewblox-ctl-lib:rpi-local docker
