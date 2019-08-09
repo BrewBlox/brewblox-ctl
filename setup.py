f137d877ca8887a4450e96d47c55be3b02d61fa7
Bob Steers <steers.bob@gmail.com>
2019-08-09 17:02:23 +0200

    use cli extras

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'requests',
-        'click',
+        'click[cli]',
         'python-dotenv[cli]',
         'pyyaml',
         'pprint'
