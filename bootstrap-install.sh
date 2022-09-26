498c9022821801934ea9ff0007fffca7a1bc9599
Bob Steers <steers.bob@gmail.com>
2022-09-26 23:23:14 +0200

    also fix the bootstrap script

--- bootstrap-install.sh
+++ bootstrap-install.sh
@@ -14,13 +14,13 @@ log_error() {
 }
 
 flush_stdin() {
-    while read -ret 0.1; do : ; done
+    while read -ret 0.1; do :; done
 }
 
 # Args:
 # - checked command
 command_exists() {
-    command -v "$@" > /dev/null 2>&1
+    command -v "$@" >/dev/null 2>&1
 }
 
 # Args:
@@ -38,10 +38,8 @@ install() {
     fi
 
     # Use BREWBLOX_DIR from env if set, otherwise find out
-    if [ -z "${BREWBLOX_DIR:-}" ]
-    then
-        if is_brewblox_dir "${PWD}"
-        then
+    if [ -z "${BREWBLOX_DIR:-}" ]; then
+        if is_brewblox_dir "${PWD}"; then
             BREWBLOX_DIR="${PWD}"
         else
             BREWBLOX_DIR="${PWD}/brewblox"
@@ -53,10 +51,8 @@ install() {
     # - Environment variable
     # - Value for previous install
     # - Default
-    if [ -z "${BREWBLOX_RELEASE:-}" ]
-    then
-        if is_brewblox_dir "${BREWBLOX_DIR}"
-        then
+    if [ -z "${BREWBLOX_RELEASE:-}" ]; then
+        if is_brewblox_dir "${BREWBLOX_DIR}"; then
             BREWBLOX_RELEASE=$(grep BREWBLOX_RELEASE "${BREWBLOX_DIR}/.env" | cut -d '=' -f2)
         else
             BREWBLOX_RELEASE="edge"
@@ -64,8 +60,7 @@ install() {
     fi
 
     # Install system packages
-    if command_exists "apt-get"
-    then
+    if command_exists "apt-get"; then
         log_info "Installing Apt packages..."
         sudo apt-get update
         sudo apt-get upgrade -y
@@ -79,8 +74,7 @@ install() {
         log_warn ""
     fi
 
-    if ! command_exists python3
-    then
+    if ! command_exists python3; then
         log_error "Python3 not found."
         log_error "Install Python >=3.6 manually, or add the existing installation to PATH."
         exit 1
@@ -90,15 +84,13 @@ install() {
     log_info "Brewblox release is \"${BREWBLOX_RELEASE}\""
 
     # Check if dir exists, but is not a brewblox dir, and is not empty
-    if [[ -d "${BREWBLOX_DIR}" ]] \
-        && ! is_brewblox_dir "${BREWBLOX_DIR}" \
-        && [ -n "$(ls -A "${BREWBLOX_DIR:?}")" ]
-    then
+    if [[ -d "${BREWBLOX_DIR}" ]] &&
+        ! is_brewblox_dir "${BREWBLOX_DIR}" &&
+        [ -n "$(ls -A "${BREWBLOX_DIR:?}")" ]; then
         log_warn "${BREWBLOX_DIR} already exists, but is not a Brewblox directory."
         flush_stdin
         read -rp "Remove all files in this directory and continue? (y/N)" response
-        if [[ "$response" =~ ^y(es)? ]]
-        then
+        if [[ "$response" =~ ^y(es)? ]]; then
             rm -rf "${BREWBLOX_DIR:?}/*"
         else
             exit 1
@@ -106,10 +98,9 @@ install() {
     fi
 
     # Ensure that the Brewblox dir is cwd
-    if [[ ${BREWBLOX_DIR} != "${PWD}" ]]
-    then
+    if [[ ${BREWBLOX_DIR} != "${PWD}" ]]; then
         mkdir -p "${BREWBLOX_DIR}"
-        pushd "${BREWBLOX_DIR}" > /dev/null
+        pushd "${BREWBLOX_DIR}" >/dev/null
     fi
 
     # Create virtual env
@@ -129,11 +120,10 @@ install() {
     # Install packages into the virtual env
     log_info "Installing Python packages..."
     python3 -m pip install pip setuptools wheel
-    python3 -m pip install --prefer-binary cryptography
-    python3 -m pip install ./brewblox-ctl.tar.gz
+    python3 -m pip install --prefer-binary ./brewblox-ctl.tar.gz
 
     # Init the .env file
-    echo "BREWBLOX_RELEASE=${BREWBLOX_RELEASE}" > .env
+    echo "BREWBLOX_RELEASE=${BREWBLOX_RELEASE}" >.env
 
     exec python3 -m brewblox_ctl install
 }
